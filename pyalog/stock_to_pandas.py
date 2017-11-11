5740ce1ac7d5e218df9131d40defe76a69c31483
Jianhau Su <jianhao.su@gmail.com>
2017-11-11 10:25:01 +0800

    refine stock_to_pandas

--- pyalog/stock_to_pandas.py
+++ pyalog/stock_to_pandas.py
@@ -4,9 +4,9 @@ from twstock import Stock
 import pandas 
 import re
 
-def twstock2pandas(stock_id):
+def stock_to_andas(stock):
     pd_stock=None
-    stock=Stock(stock_id)
+    #stock=Stock(stock_id)
     fields = ['date','capacity','turnover','open','high','low','close','change','transaction']
     dict_info={}
     for each in fields:
@@ -24,13 +24,13 @@ def twstock2pandas(stock_id):
         pd_stock[each]=pandas.Series(dict_info[each])
     return pd_stock
 
-def twstock2csv(stock_id,csv_file_name):
-    pd_stock=twstock2pandas(stock_id)
+def stock_to_csv(stock,csv_file_name):
+    pd_stock=twstock2pandas(stock)
     pd_stock.to_csv(csv_file_name,index=False)
     pass
 
-def twstock2yahooCSV(stock_id,csv_file_name):
-    pd_stock=twstock2pandas(stock_id)
+def stock_to_yahooCSV(stock,csv_file_name):
+    pd_stock=twstock2pandas(stock)
     yahoo_cols=['Date','Open','High','Low','Close','Volume','Adj Close']
     pd_stock_yahoo=pandas.DataFrame()
     pd_stock_yahoo['Date']=pd_stock.date
@@ -41,5 +41,21 @@ def twstock2yahooCSV(stock_id,csv_file_name):
     pd_stock_yahoo['Volume']=pd_stock.capacity
     pd_stock_yahoo['Adj Close']=pd_stock.close
     pd_stock_yahoo.to_csv(csv_file_name,index=False)
+    
+def stock_to_googleCSV(stock,csv_file_name):
+    pd_stock=twstock2pandas(stock)
+    yahoo_cols=['DATE','CLOSE','HIGH','LOW','OPEN','VOLUME']
+    pd_stock_yahoo=pandas.DataFrame()
+    pd_stock_yahoo['DATE']=pd_stock.date
+    pd_stock_yahoo['OPEN']=pd_stock.open
+    pd_stock_yahoo['HIGH']=pd_stock.high
+    pd_stock_yahoo['LOW']=pd_stock.low
+    pd_stock_yahoo['CLOSE']=pd_stock.close
+    pd_stock_yahoo['VOLUME']=pd_stock.capacity
+    pd_stock_yahoo.to_csv(csv_file_name,index=False)
+    
 if __name__ == "__main__":
-    twstock2yahooCSV('2618','2618.csv')
+    #stock=Stock(stock_id)
+    #stock=Stock('2618')
+    stock.fetch_from(2010,1)
+    twstock2googleCSV(stock,'2618.csv')
