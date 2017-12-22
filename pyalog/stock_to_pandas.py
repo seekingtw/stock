583cddf0035c29f9fde1267662910b9ac6cfa94a
Jianhau Su <jianhao.su@gmail.com>
2017-12-23 00:59:47 +0800

    add tw50 data and google format error

--- pyalog/stock_to_pandas.py
+++ pyalog/stock_to_pandas.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import sys
+sys.path.insert(0,"twstock-master")
 from twstock import Stock
 import pandas 
 import re
@@ -39,30 +41,31 @@ def stock_to_csv(stock,csv_file_name):
 
 def stock_to_yahooCSV(stock,csv_file_name):
     pd_stock=stock_to_pandas(stock)
-    yahoo_cols=['Date','Open','High','Low','Close','Volume','Adj Close']
+    yahoo_cols=['DATE','CLOSE','HIGH','LOW','OPEN','VOLUME','Adj Close']
     pd_stock_yahoo=pandas.DataFrame()
-    pd_stock_yahoo['Date']=pd_stock.date
-    pd_stock_yahoo['Open']=pd_stock.open
-    pd_stock_yahoo['High']=pd_stock.high
-    pd_stock_yahoo['Low']=pd_stock.low
-    pd_stock_yahoo['Close']=pd_stock.close
-    pd_stock_yahoo['Volume']=pd_stock.capacity
-    pd_stock_yahoo['Adj Close']=pd_stock.close
-    pd_stock_yahoo.to_csv(csv_file_name,index=False)
-    
-def stock_to_googleCSV(stock,csv_file_name):
-    pd_stock=stock_to_pandas(stock)
-    yahoo_cols=['DATE','CLOSE','HIGH','LOW','OPEN','VOLUME']
-    pd_stock_yahoo=pandas.DataFrame()
-    pd_stock_yahoo['DATE']=pd_stock.date.apply(lambda t:t.strftime("%d-%b-%y"))
+    pd_stock_yahoo['DATE']=pd_stock.date
     pd_stock_yahoo['OPEN']=pd_stock.open
     pd_stock_yahoo['HIGH']=pd_stock.high
     pd_stock_yahoo['LOW']=pd_stock.low
     pd_stock_yahoo['CLOSE']=pd_stock.close
     pd_stock_yahoo['VOLUME']=pd_stock.capacity
+    pd_stock_yahoo['Adj Close']=pd_stock.close
+    pd_stock_yahoo.to_csv(csv_file_name,index=False)
+    
+def stock_to_googleCSV(stock,csv_file_name):
+    pd_stock=stock_to_pandas(stock)
+    yahoo_cols=['Date','Open','High','Low','Close','Volume']
+    pd_stock_yahoo=pandas.DataFrame()
+    pd_stock_yahoo['Date']=pd_stock.date.apply(lambda t:t.strftime("%d-%b-%y"))
+    pd_stock_yahoo['Open']=pd_stock.open
+    pd_stock_yahoo['High']=pd_stock.high
+    pd_stock_yahoo['Low']=pd_stock.low
+    pd_stock_yahoo['Close']=pd_stock.close
+    pd_stock_yahoo['Volume']=pd_stock.capacity
     pd_stock_yahoo.to_csv(csv_file_name,index=False)
 
 def run_item(twid ,start, end, csv):
+    try:
         datetime_start= datetime.strptime(start,"%Y-%m-%d")
         datetime_end= datetime.strptime(end,"%Y-%m-%d")
         if csv=="":
@@ -74,7 +77,10 @@ def run_item(twid ,start, end, csv):
         #print(end)
         print("end_time(disable now): {}".format(datetime_end))
         fetch(twid,csv,datetime_start,datetime_end)
-        
+    except:
+        print("run_item except")
+    else:
+        print("run_item done")
 
 @click.command()
 @click.option('--twid', default='2330', help='tw stock id')
@@ -120,3 +126,5 @@ def fetch(twid='2330',csv="",start_time=None,end_time=datetime.now()):
 if __name__ == "__main__":
     #stock=Stock(stock_id)
     run()
+    #run_item('2317', '2010-1-1' ,'2017-12-1' ,'2317.csv')
+
